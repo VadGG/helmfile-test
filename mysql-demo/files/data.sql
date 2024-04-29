583cb2383eb2f5e9f208042df4a2723c413b82b5
Vadim G <vadm.gagarin@ugmail.com>
2024-04-29 10:55:39 -0400

    init

---
+++ mysql-demo/files/data.sql
@@ -0,0 +1,3 @@
+USE firstdb01;
+INSERT INTO customers (name, email) VALUES ('Alice Smith', 'alice@example.com'), ('Bob Johnson', 'bob@example.com');
+INSERT INTO products (name, price) VALUES ('Laptop', 999.99), ('Phone', 299.99);
